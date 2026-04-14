d193877a2806139f369d530450912c3c9bc3bc8a
timmit-exe <franketimyttv@gmail.com>
2026-04-14 07:29:08 +0000

    add basic server functionality

--- backend/Server.ts
+++ backend/Server.ts
@@ -1,4 +1,19 @@
+import express, { Request, Response } from "express";
+
 class Server {
+
+    private app: any = express();
+    private PORT: number = 3000;
+
+    Server() {
+        this.app.get("/", (req: Request, res: Response) => {
+            res.send("Hello World mit TypeScript!");
+        });
+
+        this.app.listen(this.PORT, () => {
+            console.log(`Server läuft auf http://localhost:${this.PORT}`);
+        });
+    }
 
 }
 
