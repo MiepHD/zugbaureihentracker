698ab50f4374f3d988ec63871ea8fb255cdb58a8
Fifty-4784 <christoph.gallner08@gmail.com>
2026-06-19 09:22:42 +0200

    Update SuchergebnisSeite.ts

--- frontend/suchergebnis/SuchergebnisSeite.ts
+++ frontend/suchergebnis/SuchergebnisSeite.ts
@@ -1,6 +1,30 @@
 /**
  * Christoph
  */
-let paramString = urlString.split('?')[1];
+let paramString = location.href.split('?')[1];
 let queryString = new URLSearchParams(paramString);
 const ubid = queryString.entries()[0][1];
+
+(document.querySelector("input") as HTMLInputElement).value = ubid;
+
+ const xhr = new XMLHttpRequest();
+  xhr.open('GET', "/api/getGefundeneBaureihen", true);
+  xhr.setRequestHeader('Accept', 'application/json');
+
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState !== XMLHttpRequest.DONE) {
+      return;
+    }
+
+    if (xhr.status !== 200) {
+      return;
+    }
+
+      const response = JSON.parse(xhr.responseText);
+      response.beschreibung
+      document.querySelector("p").innerHTML = response.beschreibung;
+      document.querySelector("i").innerHTML = response.name;
+     
+  };
+
+  xhr.send();
