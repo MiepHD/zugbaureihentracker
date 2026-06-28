aa206c074f25d45c33ebf5612c988d8782fc819e
MiepHD <github-private-gu37uf@froxot.de>
2026-06-28 08:15:13 +0200

    Baureihen werden jetzt gruppiert auf der Baureihen-Übersicht

--- frontend/baureihen/BaureihenSeite.ts
+++ frontend/baureihen/BaureihenSeite.ts
@@ -3,10 +3,20 @@ new XHR().get("/api/getBaureihen", (response: any) => {
     showError(response);
     return;
   }
-  const list = document.querySelector("ul") as HTMLUListElement;
+  let list = document.querySelector("ul") as HTMLUListElement;
+  let group = "ABC";
   for (const baureihe of response) {
+    if (!(baureihe.ubid as string).startsWith(group)) {
+      group = (baureihe.ubid as string).slice(0, 3);
+      const details = document.createElement("details");
+      details.innerHTML = `<summary>${group}</summary>`;
+      list = document.createElement("ul");
+      list.classList.add("baureihen");
+      details.appendChild(list);
+      (document.querySelector("main") as HTMLElement).appendChild(details);
+    }
     const elem = document.createElement("li");
-    elem.innerHTML = `<a href="/editor?ubid=${baureihe.ubid}">${baureihe.ubid}</a> <i>${baureihe.name}</i> <form action="/api/removeBaureihe" method="post" style="display: inline-block;"> <input name="ubid" value="${baureihe.ubid}" style="display: none"><button style="background-color: red">Löschen</button></form>`;
-    list.appendChild(elem);
+      elem.innerHTML = `<a href="/editor?ubid=${baureihe.ubid}">${baureihe.ubid}</a> <i>${baureihe.name}</i> <form action="/api/removeBaureihe" method="post" style="display: inline-block;"> <input name="ubid" value="${baureihe.ubid}" style="display: none"><button style="background-color: red">Löschen</button></form>`;
+      list.appendChild(elem);
   }
 });
