25a58bc13e3e316491e2daa8adadb8e9c15cb12e
MiepHD <github-private-gu37uf@froxot.de>
2026-07-01 17:52:45 +0200

    Auf der Invite-Seite wird das Passwortfeld nur noch angezeigt, wenn man nicht Admin ist

---
+++ frontend/invite/InviteSeite.ts
@@ -0,0 +1,10 @@
+new XHR().get("/api/isElevated", (response: any) => {
+  if (typeof response == "string" && !response.startsWith("401")) {
+    messageHandler.showError(response);
+    return;
+  }
+  if (response && response.isElevated) {
+    (document.getElementById("passwort") as HTMLElement).style.setProperty("display", "none");
+    (document.querySelector("label[for=passwort]") as HTMLElement).style.setProperty("display", "none");
+  }
+});
