6b4b6ee409d2d39ca5161c81ee0bc80eaf0c184b
MiepHD <github-private-gu37uf@froxot.de>
2026-07-03 12:03:45 +0200

    Man kann jetzt Baureihen filtern Closes #36

--- frontend/baureihen/BaureihenSeite.ts
+++ frontend/baureihen/BaureihenSeite.ts
@@ -3,20 +3,5 @@ new XHR().get("/api/baureihe/json/getall", (response: any) => {
     messageHandler.showError(response);
     return;
   }
-  let list = document.querySelector("ul") as HTMLUListElement;
-  let group = "ABC";
-  for (const baureihe of response) {
-    if (!(baureihe.ubid as string).startsWith(group)) {
-      group = (baureihe.ubid as string).slice(0, 3);
-      const details = document.createElement("details");
-      details.innerHTML = `<summary>${group}</summary>`;
-      list = document.createElement("ul");
-      list.classList.add("baureihen");
-      details.appendChild(list);
-      (document.querySelector("main") as HTMLElement).appendChild(details);
-    }
-    const elem = document.createElement("li");
-      elem.innerHTML = `<a href="/editor?ubid=${baureihe.ubid}">${baureihe.ubid}</a> <i>${baureihe.name}</i> <form action="/api/baureihe/web/remove" method="post" style="display: inline-block;"> <input name="ubid" value="${baureihe.ubid}" style="display: none"><button style="background-color: red">Löschen</button></form>`;
-      list.appendChild(elem);
-  }
+  renderBaureihen(response, '<a href="/editor?ubid=%s">%s</a> <form action="/api/baureihe/web/remove" method="post" style="display: inline-block;"> <input name="ubid" value="%s" style="display: none"><button style="background-color: red">Löschen</button></form>');
 });
--- frontend/general/RequestBaureihen.ts
+++ frontend/general/RequestBaureihen.ts
@@ -3,7 +3,7 @@ function getBaureihenEndpoint(): string {
   return '/api/aktivitaet/json/getgefunden';
 }
 
-function renderBaureihen(items: unknown): void {
+function renderBaureihen(items: unknown, additionalContent = ""): void {
   const ul = document.getElementById('liste');
   if (!ul) {
     return;
@@ -15,6 +15,19 @@ function renderBaureihen(items: unknown): void {
   }
 
   let list = ul;
+  const input = document.createElement("input");
+  input.addEventListener("input", () => {
+    for (const group of document.querySelectorAll('#liste > details')) {
+      for (const elem of group.children[1].children) {
+        if (elem.textContent?.startsWith(input.value)) {
+          (elem as HTMLElement).style.removeProperty("display");
+        } else {
+          (elem as HTMLElement).style.setProperty("display", "none");
+        }
+      }
+    }
+  });
+  list.appendChild(input);
   let group = "ABC";
   for (const baureihe of items) {
     if (!(baureihe.ubid as string).startsWith(group)) {
@@ -27,7 +40,7 @@ function renderBaureihen(items: unknown): void {
       (document.getElementById("liste") as HTMLElement).appendChild(details);
     }
     const li = document.createElement('li');
-    li.innerHTML = `<a href="/suchergebnis?ubid=${baureihe.ubid}">${baureihe.ubid}</a>`;
+    li.innerHTML = `<a href="/suchergebnis?ubid=${baureihe.ubid}">${baureihe.ubid}</a>${additionalContent.replaceAll("%s", baureihe.ubid)}`;
     list.prepend(li);
   }
 }
