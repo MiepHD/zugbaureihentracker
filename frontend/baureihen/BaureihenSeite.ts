e6435c1df489434a6f280f46a7f8bb5e199be376
MiepHD <github-private-gu37uf@froxot.de>
2026-07-03 16:33:28 +0200

    Added space between ubid and Ändern

--- frontend/baureihen/BaureihenSeite.ts
+++ frontend/baureihen/BaureihenSeite.ts
@@ -3,5 +3,5 @@ new XHR().get("/api/baureihe/json/getall", (response: any) => {
     messageHandler.showError(response);
     return;
   }
-  renderBaureihen(response, '<a href="/editor?ubid=%s">Ändern</a> <form action="/api/baureihe/web/remove" method="post" style="display: inline-block;"> <input name="ubid" value="%s" style="display: none"><button style="background-color: red">Löschen</button></form>');
+  renderBaureihen(response, ' <a href="/editor?ubid=%s">Ändern</a> <form action="/api/baureihe/web/remove" method="post" style="display: inline-block;"> <input name="ubid" value="%s" style="display: none"><button style="background-color: red">Löschen</button></form>');
 });
