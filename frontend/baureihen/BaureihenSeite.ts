e5e701c5351d164aae56e7f6a7525eea18863d96
MiepHD <github-private-gu37uf@froxot.de>
2026-07-09 21:10:41 +0200

    Icons umgewandelt

--- frontend/baureihen/BaureihenSeite.ts
+++ frontend/baureihen/BaureihenSeite.ts
@@ -3,5 +3,5 @@ new XHR().get("/api/baureihe/json/getall", (response: any) => {
     messageHandler.showError(response);
     return;
   }
-  renderBaureihen(response, ' <a href="/editor?ubid=%s"><img class="icon" src="/assets/pencil-svgrepo-com.svg" alt="zurückziehen" /></a> <form action="/api/baureihe/web/remove" method="post" style="display: inline-block;"> <input name="ubid" value="%s" style="display: none"><button style="background-color: red"><img class="icon" src="/assets/x-alt-svgrepo-com.svg" alt="löschen" /></button></form>');
+  renderBaureihen(response, ' <a href="/editor?ubid=%s" aria-label="zurückziehen" class="icon pencil small"></a> <form action="/api/baureihe/web/remove" method="post" style="display: inline-block;"> <input name="ubid" value="%s" style="display: none"><button style="background-color: red" aria-label="löschen" class="icon small x"></button></form>');
 });
