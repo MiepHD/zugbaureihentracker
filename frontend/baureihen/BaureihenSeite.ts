e6eff439f79e82909843968a7fe85d4f0c38c405
Yazazuyo <63968466+MiepHD@users.noreply.github.com>
2026-07-10 11:21:57 +0200

    Basic Gestaltung hinzugefügt (#62)
    * Basic Farben und Icons hinzugefügt
    
    * Erster Versuch das Layout auf die gesamte Seite auszuweiten
    
    * Changed color palette
    
    * Icons umgewandelt
    
    * Alignment und Größe der Symbole angepasst
    
    * Inhalte ordentlich zentriert
    Padding verringert, dass besser auf mobilen Geräten angezeigt werden kann
    Unnötigen Link von Anfragenseite entfernt
    Knöpfe lesbar gemacht
    Überschrift gekürzt für besseren Support von kleinen Bildschirmen
    Unnötige UUID aus Benutzerliste entfernt

--- frontend/baureihen/BaureihenSeite.ts
+++ frontend/baureihen/BaureihenSeite.ts
@@ -3,5 +3,5 @@ new XHR().get("/api/baureihe/json/getall", (response: any) => {
     messageHandler.showError(response);
     return;
   }
-  renderBaureihen(response, ' <a href="/editor?ubid=%s">Ändern</a> <form action="/api/baureihe/web/remove" method="post" style="display: inline-block;"> <input name="ubid" value="%s" style="display: none"><button style="background-color: red">Löschen</button></form>');
+  renderBaureihen(response, ' <a href="/editor?ubid=%s" aria-label="zurückziehen" class="icon pencil small"></a> <form action="/api/baureihe/web/remove" method="post" style="display: inline-block;"> <input name="ubid" value="%s" style="display: none"><button style="background-color: red" aria-label="löschen" class="icon small x"></button></form>');
 });
--- frontend/freundschaftsanfragen/FreundschaftsanfragenSeite.ts
+++ frontend/freundschaftsanfragen/FreundschaftsanfragenSeite.ts
@@ -5,12 +5,12 @@ const fill2 = (data: any) => {
     const ausgehend = document.getElementById("ausgehend");
     for (const user of data.eingehend) {
           const li = document.createElement("li");
-          li.innerHTML = `<b>${user.eingehendeAnfragen.name}</b> <form action="/api/freundesliste/web/akzeptiereanfrage" method="post" style="display:inline-block"><input name="uuid" value="${user.von}" style="display: none;"><button>Anfrage akzeptieren</button></form><form action="/api/freundesliste/web/ablehnenanfrage" method="post" style="display:inline-block"><input name="uuid" value="${user.von}" style="display: none;"><button>Anfrage ablehnen</button></form>`;
+          li.innerHTML = `<b>${user.eingehendeAnfragen.name}</b>&nbsp;&nbsp;&nbsp;<form action="/api/freundesliste/web/akzeptiereanfrage" method="post" style="display:inline-block"><input name="uuid" value="${user.von}" style="display: none;"><button aria-label="akzeptieren" class="icon small usercheck"></button></form>&nbsp;&nbsp;&nbsp;<form action="/api/freundesliste/web/ablehnenanfrage" method="post" style="display:inline-block"><input name="uuid" value="${user.von}" style="display: none;"><button aria-label="ablehnen" class="icon small userx"></button></form>`;
           eingehend?.appendChild(li);
     }
     for (const user of data.ausgehend) {
           const li = document.createElement("li");
-          li.innerHTML = `<b>${user.ausgehendeAnfragen.name}</b><form action="/api/freundesliste/web/abortanfrage" method="post" style="display:inline-block"><input name="uuid" value="${user.zu}" style="display: none;"><button>Anfrage zurückziehen</button></form>`
+          li.innerHTML = `<b>${user.ausgehendeAnfragen.name}</b>&nbsp;&nbsp;&nbsp;<form action="/api/freundesliste/web/abortanfrage" method="post" style="display:inline-block"><input name="uuid" value="${user.zu}" style="display: none;"><button aria-label="zurückziehen" class="icon small userx"></button></form>`
           ausgehend?.appendChild(li);
     }
 }
