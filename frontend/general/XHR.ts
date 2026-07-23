623e304801b3ea0cf7b66165632f176f690aea61
MiepHD <github-private-gu37uf@froxot.de>
2026-07-23 18:44:21 +0200

    Statuscode 0 auch zu post-Request hinzugefügt

--- frontend/general/XHR.ts
+++ frontend/general/XHR.ts
@@ -4,9 +4,33 @@ class XHR {
         this.xhr = new XMLHttpRequest();
     }
     
-    get(endpoint: string, callback: Function) {
+    get(endpoint: string, callback: (response: any) => void) {
         this.xhr.open('GET', endpoint, true);
         this.xhr.setRequestHeader('Accept', 'application/json');
+        this.handleAnswer(callback);
+        this.xhr.send();
+    }
+
+    post(endpoint: string, data: any, callback: (response: any) => void) {
+        this.xhr.open('POST', endpoint, true);
+        // Teilt dem Server mit, dass wir JSON senden und erwarten
+        this.xhr.setRequestHeader('Content-Type', 'application/json');
+        this.xhr.setRequestHeader('Accept', 'application/json');
+        
+        this.handleAnswer(callback);
+
+        if (data instanceof FormData) {
+            const newdata: Record<string, any> = {};
+            data.forEach((value, key) => {
+                newdata[key] = value;
+            });
+            data = newdata;
+        }
+        
+        this.xhr.send(JSON.stringify(data));
+    }
+
+    private handleAnswer(callback: (response: any) => void) {
         this.xhr.onreadystatechange = () => {
             if (this.xhr.readyState !== XMLHttpRequest.DONE) {
                 return;
@@ -23,39 +47,6 @@ class XHR {
             }
             callback(response);
         };
-        this.xhr.send();
-    }
-
-    post(endpoint: string, data: any, callback: Function) {
-        this.xhr.open('POST', endpoint, true);
-        // Teilt dem Server mit, dass wir JSON senden und erwarten
-        this.xhr.setRequestHeader('Content-Type', 'application/json');
-        this.xhr.setRequestHeader('Accept', 'application/json');
-        
-        this.xhr.onreadystatechange = () => {
-            if (this.xhr.readyState !== XMLHttpRequest.DONE) {
-                return;
-            }
-
-            let response = null;
-            try {
-                response = JSON.parse(this.xhr.responseText);
-            } catch {
-                response = this.xhr.responseText;
-            }
-            
-            callback(response);
-        };
-
-        if (data instanceof FormData) {
-            const newdata: Record<string, any> = {};
-            data.forEach((value, key) => {
-                newdata[key] = value;
-            });
-            data = newdata;
-        }
-        
-        this.xhr.send(JSON.stringify(data));
     }
 }
 
